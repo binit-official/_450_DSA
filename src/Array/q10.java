1475e9ba493f5a0f65bc71ae8e771c5b745da606
Binit Kuanar <phantombhaiyaa@gmail.com>
2024-02-16 20:28:06 +0530

    Preparation of DSA

--- src/Array/q10.java
+++ src/Array/q10.java
@@ -1,5 +1,6 @@
 package Array;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class q10 {
@@ -18,6 +19,16 @@ public class q10 {
 }
 
     private static void insertionsort(int[] arr) {
-
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
